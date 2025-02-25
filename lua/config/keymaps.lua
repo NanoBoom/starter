70420f264edbd709eae1290978892a2476d6673e
范立洲 <fanlizhou@yunqilaohe.com>
2025-02-25 15:06:45 +0800

    feat: enhance configuration with new plugins and keymap options
    - Add two new AI plugins to the extras list in the configuration
    - Replace a custom utility plugin with a different one in the extras list
    - Introduce several new keymaps for various actions in insert mode
    - Add new options related to clipboard handling and mouse movement events in the configuration
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<C-d>", "<C-o>diw", { desc = "Delete inner word" })
+vim.keymap.set("i", "<C-u>", "<C-o>dI", { desc = "Delete to beginning of line" })
+vim.keymap.set("i", "<C-k>", "<C-o>D", { desc = "Delete to end of line" })
+vim.keymap.set("i", "<C-b>", "<Left>", { desc = "Move cursor left" })
+vim.keymap.set("i", "<C-f>", "<Right>", { desc = "Move cursor right" })
+vim.keymap.set("i", "<C-a>", "<Esc>^i", { desc = "Move to beginning of line and insert" })
+vim.keymap.set("i", "<C-n>", "<Down>", { desc = "Move cursor down" })
+vim.keymap.set("i", "<C-p>", "<Up>", { desc = "Move cursor up" })
+vim.keymap.set("i", "<C-s>", "<ESC>:w<CR>", { desc = "Save file" })
+vim.keymap.set("i", "<C-j>", "<C-o>o", { desc = "Insert new line below" })
+vim.keymap.set("i", "<C-l>", "<C-o>O", { desc = "Insert new line above" })
+
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete without copy" })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without copy" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mousemoveevent = true
+vim.opt.jumpoptions = "stack"
+
+vim.g.clipboard = {
+  name = "uniclipboard",
+  copy = {
+    ["+"] = "clipboard-provider copy",
+    ["*"] = "clipboard-provider copy",
+  },
+  paste = {
+    ["+"] = "clipboard-provider paste",
+    ["*"] = "clipboard-provider paste",
+  },
+  cache_enabled = 1,
+}
