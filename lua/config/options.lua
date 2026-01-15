2987cb3ed996c6c8462f883967474694d77e8f82
NanoBoom <cinuor@gmail.com>
2026-01-15 14:18:37 +0800

    feat: remove clipboard option
    - Remove clipboard option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "unnamedplus"
 vim.opt.mousemoveevent = true
 vim.opt.jumpoptions = "stack"
 
