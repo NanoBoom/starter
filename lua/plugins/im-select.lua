18d214af0d19cae8fccf4f8ab806b62102d85b35
NanoBoom <cinuor@gmail.com>
2026-06-03 19:00:37 +0800

    feat: update im-select

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,10 +1,9 @@
 return {
   "NanoBoom/im-select.nvim",
   event = "VeryLazy",
-  config = function()
-    local opts = {
-      set_default_events = { "InsertLeave", "CmdlineLeave", "FocusGained" },
-    }
+  opts = function(_, opts)
+    -- opts.set_default_events = { "InsertLeave", "CmdlineLeave", "FocusGained" }
+    opts.set_default_events = { "InsertLeave", "CmdlineLeave" }
     if vim.fn.has("macunix") == 1 then
       opts.default_im_select = "com.apple.keylayout.ABC"
       opts.insert_im = "im.rime.inputmethod.Squirrel.Hans"
@@ -12,7 +11,6 @@ return {
       opts.default_im_select = "im-select-mspy.exe"
       opts.insert_im = "中文模式"
     end
-    -- Linux: 不设置 default_im_select / insert_im，使用插件默认值
-    require("im_select").setup(opts)
+    -- Linux: 不设置上述参数，使用插件默认值
   end,
 }
